fc9249b9af00e2b9c5e8ac81322fe7ae27b67b52
Re'em Halamish <reemh@google.com>
2021-03-21 19:42:10 +0200

    ~~~ initial commit ~~~

---
+++ app/src/main/java/exercise/android/reemh/todo_items/MainActivity.java
@@ -0,0 +1,14 @@
+package exercise.android.reemh.todo_items;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
