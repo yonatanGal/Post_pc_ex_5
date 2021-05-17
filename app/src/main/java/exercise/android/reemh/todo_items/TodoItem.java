f5f21a7e6e3fab4dfcba5ca9588c78852fde7ace
yonatanGal <jhonygal@gmail.com>
2021-05-17 14:51:31 +0300

    added an implemntation of ToDoItem, started the TodoItemsHolderImpl

--- app/src/main/java/exercise/android/reemh/todo_items/TodoItem.java
+++ app/src/main/java/exercise/android/reemh/todo_items/TodoItem.java
@@ -3,5 +3,35 @@ package exercise.android.reemh.todo_items;
 import java.io.Serializable;
 
 public class TodoItem implements Serializable {
-  // TODO: edit this class as you want
+    private final String data;
+    private final String timeCreated;
+    private boolean isInProgress;
+
+    public TodoItem(String data, String timeCreated, boolean isInProgress)
+  {
+      this.data = data;
+      this.timeCreated = timeCreated;
+      this.isInProgress = isInProgress;
+  }
+
+  public String getData()
+  {
+      return this.data;
+  }
+
+  public String getTimeCreated()
+  {
+      return this.timeCreated;
+  }
+
+  public boolean getIsInProgress()
+  {
+      return this.isInProgress;
+  }
+
+  public void changeProgress()
+  {
+      this.isInProgress = !this.isInProgress;
+  }
+
 }
--- app/src/main/java/exercise/android/reemh/todo_items/TodoItemsHolderImpl.java
+++ app/src/main/java/exercise/android/reemh/todo_items/TodoItemsHolderImpl.java
@@ -1,20 +1,37 @@
 package exercise.android.reemh.todo_items;
 
 import java.util.List;
+import java.sql.Timestamp;
 
 // TODO: implement!
 public class TodoItemsHolderImpl implements TodoItemsHolder {
+  
+
   @Override
   public List<TodoItem> getCurrentItems() { return null; }
 
   @Override
-  public void addNewInProgressItem(String description) { }
+  public void addNewInProgressItem(String description) {
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    TodoItem newInProgressItem = new TodoItem(description, timestamp.toString(), true);
+
+  }
 
   @Override
-  public void markItemDone(TodoItem item) { }
+  public void markItemDone(TodoItem item) {
+    if (item.getIsInProgress())
+    {
+      item.changeProgress();
+    }
+  }
 
   @Override
-  public void markItemInProgress(TodoItem item) {}
+  public void markItemInProgress(TodoItem item) {
+    if (!item.getIsInProgress())
+    {
+      item.changeProgress();
+    }
+  }
 
   @Override
   public void deleteItem(TodoItem item) { }
